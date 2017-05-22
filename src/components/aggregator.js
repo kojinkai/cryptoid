42a29fa99ecad8a5a5647647062c1d849081818d
Lewis Nixon <lewcfn@gmail.com>
2017-05-22 11:39:12 +0100

    add a components directory and an aggregator component

---
+++ src/components/aggregator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Aggregator extends Component {
+  render() {
+    return (
+      <div className="aggregator">
+        <div className="aggregator__title">+ £300</div>
+      </div>
+    );
+  }
+}
+
+export default Aggregator;
