882336b22c2bb833ed462230033b6a06d648910e
Lewis Nixon <lewcfn@gmail.com>
2017-05-29 10:44:35 +0100

    Call coinbase api and show results in aggregator

--- src/components/aggregator.js
+++ src/components/aggregator.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import coinbaseApi from '../api/api';
 
 class Aggregator extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        native_balance: {
+          amount: 0
+        }
+      },
+      loaded: false
+    };
+  }
+  
+  componentDidMount() {
+    coinbaseApi.getAccount()
+      .then(response => response.json())
+      .then(data => {
+        console.log('data is: ', data.data.native_balance.amount);
+
+        this.setState({
+          data: data.data,
+          loaded: true
+        });
+      });
+    console.log('did mount son');
+  }
+
   render() {
     return (
       <div className="aggregator">
-        <div className="aggregator__title">+ £300</div>
+        <div className="aggregator__title">{this.state.data.native_balance.amount}</div>
       </div>
     );
   }
