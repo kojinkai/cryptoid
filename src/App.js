574e3adc7767cb1ac427a4d92879d13e7bd99407
Lewis Nixon <lewcfn@gmail.com>
2017-05-29 14:20:01 +0100

    Pass account data via props to aggregator component from app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import coinbaseApi from './api/api';
 import Aggregator from './components/aggregator'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        native_balance: {
+          amount: 0.00,
+          currency: 'GBP'
+        }
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to Cryptoid</h2>
         </div>
-        <Aggregator />
+        <Aggregator account={this.state.data}/>
       </div>
     );
+  }
+
+  componentDidMount() {
+    coinbaseApi.getAccount()
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          data: data.data,
+          loaded: true
+        });
+      });
   }
 }
 
--- src/components/aggregator.js
+++ src/components/aggregator.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react';
-import coinbaseApi from '../api/api';
 
 class Aggregator extends Component {
-  
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: {
-        native_balance: {
-          amount: 0
-        }
-      },
-      loaded: false
-    };
-  }
-  
-  componentDidMount() {
-    coinbaseApi.getAccount()
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          data: data.data,
-          loaded: true
-        });
-      });
-  }
-
   render() {
     return (
       <div className="aggregator">
-        <div className="aggregator__title">{this.state.data.native_balance.amount}</div>
+        <div className="aggregator__title">
+          {this.props.account.native_balance.currency === 'GBP' ? '£' : '$'}
+          {this.props.account.native_balance.amount}
+        </div>
       </div>
     );
   }
