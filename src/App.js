2fe2634ed65e7e081d8ffee702c3c45e36d4ce47
Lewis Nixon <lewcfn@gmail.com>
2017-06-18 10:37:34 +0100

    send profit and percentages down via props to aggregator

--- src/App.js
+++ src/App.js
@@ -8,14 +8,31 @@ class App extends Component {
     super(props);
 
     this.state = {
-      account: {
-        native_balance: {
-          amount: 0.00,
-          currency: 'GBP'
-        }
-      },
-      buys: []
+      pl: '',
+      percentage: '',
+      inprofit: true
     };
+  }
+
+  _extractProfit(state) {
+    // move this logic to the aggregator, dummy
+    // just send account and buys down
+    function extractSum(accumulator, currentValue) {
+      return accumulator + parseFloat(currentValue.total.amount);
+    }
+
+    function extractPercentageGrowth(startingValue, currentValue) {
+      return (currentValue / startingValue * 100).toFixed(2);
+    }
+
+    const sumBuys = state.buys.reduce(extractSum, 0);
+    const balance = parseFloat(state.account.native_balance.amount);
+    const percentage = extractPercentageGrowth(sumBuys, balance);
+    const pl         = balance - sumBuys;
+    const inProfit   = (pl > 0);
+
+    return {pl, percentage, inProfit};
+
   }
 
   render() {
@@ -24,7 +41,11 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to Cryptoid</h2>
         </div>
-        <Aggregator account={this.state.account} buys={this.state.buys}/>
+        <Aggregator 
+          pl={this.state.pl}
+          percentage={this.state.percentage} 
+          inprofit={this.state.inprofit}
+        />
       </div>
     );
   }
@@ -32,19 +53,29 @@ class App extends Component {
   componentDidMount() {
     coinbaseApi.getAccount()
       .then(response => response.json())
-      .then(data => {
+      .then(accountData => {
         this.setState({
-          account: data.data
+          account: accountData.data
+        });
+      })
+      .then(() => coinbaseApi.getBuys())
+      .then(response => response.json())
+      .then(buysData => {
+        this.setState({
+          buys: buysData.data
+        });
+      })
+      .then(() => this.state)
+      .then(this._extractProfit)
+      .then(profitState => {
+        this.setState({
+          pl: profitState.pl,
+          percentage: profitState.percentage,
+          inprofit: profitState.inprofit
         });
       });
 
-    coinbaseApi.getBuys()
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          buys: data.data
-        });
-      });
+
   }
 }
 
--- src/components/aggregator.js
+++ src/components/aggregator.js
@@ -4,17 +4,11 @@ class Aggregator extends Component {
   render() {
     return (
       <div className="aggregator">
-        <div className="aggregator__title">
-          {this.props.account.native_balance.currency === 'GBP' ? '£' : '$'}
-          {this.props.account.native_balance.amount}
-        </div>
-        <div className="buys">
-          <ul className="buys__list">
-            {this.props.buys.map(buy => (
-              <li key={buy.id}>{buy.total.amount}</li>
-            ))}
-          </ul>          
-        </div>
+        <div>BTC</div>
+        <h1 className="aggregator__title">
+          +£{this.props.pl}
+        </h1>
+        <h2>Up {this.props.percentage}</h2>
       </div>
     );
   }
