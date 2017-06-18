4940ef2d218243d81c1f675742b42a89e235029b
lewisnixon <lewcfn@gmail.com>
2017-06-18 10:45:26 +0100

    feat(coinbase): Add API client for account and buys endpoints (#1)
    * add a components directory and an aggregator component
    
    * Add .nvmrc
    
    * Add account.txt to .gitignore.  Used for capturing API output & debugging
    
    * Call coinbase api and show results in aggregator
    
    * Add a basic API service for getting data from coinbase
    
    * Pass account data via props to aggregator component from app
    
    * remove height CSS prop from app header
    
    * Show buys in aggregator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import coinbaseApi from './api/api';
+import Aggregator from './components/aggregator'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pl: '',
+      percentage: '',
+      inprofit: true
+    };
+  }
+
+  _extractProfit(state) {
+    // move this logic to the aggregator, dummy
+    // just send account and buys down
+    function extractSum(accumulator, currentValue) {
+      return accumulator + parseFloat(currentValue.total.amount);
+    }
+
+    function extractPercentageGrowth(startingValue, currentValue) {
+      return (currentValue / startingValue * 100).toFixed(2);
+    }
+
+    const sumBuys = state.buys.reduce(extractSum, 0);
+    const balance = parseFloat(state.account.native_balance.amount);
+    const percentage = extractPercentageGrowth(sumBuys, balance);
+    const pl         = balance - sumBuys;
+    const inProfit   = (pl > 0);
+
+    return {pl, percentage, inProfit};
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Cryptoid</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Aggregator 
+          pl={this.state.pl}
+          percentage={this.state.percentage} 
+          inprofit={this.state.inprofit}
+        />
       </div>
     );
+  }
+
+  componentDidMount() {
+    coinbaseApi.getAccount()
+      .then(response => response.json())
+      .then(accountData => {
+        this.setState({
+          account: accountData.data
+        });
+      })
+      .then(() => coinbaseApi.getBuys())
+      .then(response => response.json())
+      .then(buysData => {
+        this.setState({
+          buys: buysData.data
+        });
+      })
+      .then(() => this.state)
+      .then(this._extractProfit)
+      .then(profitState => {
+        this.setState({
+          pl: profitState.pl,
+          percentage: profitState.percentage,
+          inprofit: profitState.inprofit
+        });
+      });
+
+
   }
 }
 
