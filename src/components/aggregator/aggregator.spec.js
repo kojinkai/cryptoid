b21d8cbabb1aacc22186e3005b3ca7dbe0609fee
Lewis Nixon <lewcfn@gmail.com>
2017-06-24 19:35:39 +0100

    Add green path tests to the aggregator component

--- src/components/aggregator/aggregator.spec.js
+++ src/components/aggregator/aggregator.spec.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Aggregator from './aggregator';
 import purchases from '../../mocks/purchases';
-import accounts from '../../mocks/purchases';
+import accounts from '../../mocks/accounts';
 
 describe('when rendering the aggregator component', () => {
-  it('it renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Aggregator
-      account={accounts[0]}
-      purchases={purchases}
-      isLoading={ false } />, div);
+  it('it should render an aggregate price', () => {
+    const wrapper = shallow(<Aggregator account={accounts[0]} purchases={purchases} isLoading={ false } />);
+    expect(wrapper.find('.aggregator__pl-currency').find('span').text()).toBe('£193.94');
+  });
+  it('it should render an total paid price', () => {
+    const wrapper = shallow(<Aggregator account={accounts[0]} purchases={purchases} isLoading={ false } />);
+    expect(wrapper.find('.aggregator__metadata').childAt(0).text()).toBe('£606.06 paid');
+  });
+  it('it should render a balance now', () => {
+    const wrapper = shallow(<Aggregator account={accounts[0]} purchases={purchases} isLoading={ false } />);
+    expect(wrapper.find('.aggregator__metadata').childAt(1).text()).toBe('£800 now');
+  });
+  it('it should render a percentage tilt', () => {
+    const wrapper = shallow(<Aggregator account={accounts[0]} purchases={purchases} isLoading={ false } />);
+    expect(wrapper.find('.aggregator__metadata').childAt(2).text()).toBe('+132.00%');
   });
 });
