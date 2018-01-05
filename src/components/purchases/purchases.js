e252c043c111b686f2ac8fa5bccdd6df5868e22e
Lewis Nixon <lewcfn@gmail.com>
2018-01-05 13:59:38 +0100

    add a purchases component

---
+++ src/components/purchases/purchases.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import './purchases.css';
+
+class Purchases extends Component {
+  render() {
+
+    return (
+      <section className="purchases">
+      </section>
+    );
+  }
+}
+
+
+export default Purchases;
